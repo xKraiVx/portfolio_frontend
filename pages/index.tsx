5cac56b62636d5e7a7c01ae505ba8c1fbef6c9a5
KraiV <kryvdaandrei@gmail.com>
2022-08-22 08:56:15 +0300

    test with SWR

--- pages/index.tsx
+++ pages/index.tsx
@@ -8,10 +8,12 @@ import { homePageApi } from '@requests/home-page.api';
 import { DefaultLayout } from '@layouts/default-layout/default-layout';
 
 import { selectTemplate } from '@features/home/components/select-template';
+import { homePageHooks } from 'api/hooks/home-page.api-hooks';
 
-const Home: FunctionComponent<HomePNormalized> = ({ title, header, widgets }) => {
+const Home: FunctionComponent/* <HomePNormalized> */ = (/* { title, header, widgets } */) => {
 
-
+  const { data } = homePageHooks.useGetHomePageQuery();
+  const { title, header, widgets } = data || {};
   return (
     <DefaultLayout headerData={header} title={title}>
       <div>
@@ -29,7 +31,7 @@ const Home: FunctionComponent<HomePNormalized> = ({ title, header, widgets }) =>
 
 export default Home
 
-export const getStaticProps: GetStaticProps = async () => {
+/* export const getStaticProps: GetStaticProps = async () => {
 
   const {
     title,
@@ -46,3 +48,4 @@ export const getStaticProps: GetStaticProps = async () => {
     revalidate: 1
   }
 }
+ */
